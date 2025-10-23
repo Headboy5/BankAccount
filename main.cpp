62f23e00009c27e13ba0ed56f4d0fde389d2a8f0
Headboy5 <stephenajclark@proton.me>
2025-10-23 19:44:54 +0100

    Add deposit and withdraw methods to BankAccount class
    Modified the `BankAccount` class:
    - Made `balance` and `accountHolderName` public.
    - Added `accountNumber` and `sortCode` members with default values.
    - Implemented `dep` method for depositing money.
    - Implemented `with` method for withdrawing money.
    
    Updated `main` function:
    - Created a `BankAccount` object and initialized its properties.
    - Demonstrated the `dep` and `with` methods for deposit and withdrawal.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,45 @@
 #include <string>
 
 class BankAccount {
-private:
-    double balance;
-public:
-    std::string accountHolderName;
+    public:
+        double balance = 0;
+        std::string accountHolderName;
+	    int accountNumber = 0;
+	    int sortCode = 0;
+        
+		// Deposit function
+        void dep() {
+            double amount;
+            std::cout << "Enter amount to deposit: ";
+            std::cin >> amount;
+            balance += amount;
+            std::cout << "Deposited: " << amount << ", New Balance: " << balance << std::endl;
+        }
 
-
-
-}
+		// Withdraw function
+        void with() {
+            double amount;
+            std::cout << "Enter amount to withdraw: ";
+            std::cin >> amount;
+            if (amount > balance) {
+                std::cout << "Insufficient funds!" << std::endl;
+            }
+            else {
+                balance -= amount;
+            }
+            std::cout << "Withdrawing: " << amount << ", New Balance " << balance << std::endl;
+        }          
+};
 
 int main() {
+	BankAccount myAccount;
 
+	myAccount.balance = 1000.0;
+	myAccount.accountHolderName = "John Doe";
+	myAccount.accountNumber = 12345678;
+	myAccount.sortCode = 112233;
+    myAccount.dep();
+	myAccount.with();
+
+    return 0;
 }
